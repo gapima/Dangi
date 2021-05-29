453e491011f6e828de1aa50c45af51cf9b1623ac
Gabriel Pinto Lima <gabriel.lima@movats.com.br>
2021-05-29 08:57:32 -0300

    new: released app website file

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+app_name = "Dangi"
+
+@app.route('/')
+def index():
+    return render_template('index.html', page_title="Test") 
+
+@app.route('/login')
+def login():
+    return render_template('login.html') 
+
+@app.route('/register')
+def register():
+    return render_template('register.html') 
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html') 
